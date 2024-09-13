a5f968800b3c7537117f2646fc68cb5cd1053cc0
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-13 09:58:13 -0600

    Add files via upload

--- funcionesv2.py
+++ funcionesv2.py
@@ -1,20 +1,33 @@
 print("Funciones version 2")
 print("Olave Cruz")
-celulares=["Uno","Dos","Tres"]
+celulares=("iPhone","Xiaomi","LG")
 razasdeperro=["roguaile","bulterie","buldo"]
-
-
+#Set
+set1 = (("José José", "Canciones interpretadas:", 34, "Edad:", 40, "Hombre"))
+#Tupla
 def verlista(telefonos):
     for uncelular in telefonos: print (uncelular)
-    
-    
+#Lista
     def verlista2(perros): 
         for nombres in perros: print (nombres)
 
-
-print("Imprime celulares")
+#Tupla
+print("Imprime celulares en forma de Tupla")
 verlista(celulares)
-
-
-print("Imprime razas de perros")
+#Lista
+print("Imprime razas de perros en forma de Lista")
 verlista(razasdeperro)
+#Set
+print("Imprime los datos de una persona en forma de Set")
+verlista(set1)
+#Diccionario
+print("Imprime un diccionario")
+thisdict = {
+  "Marca": "McLaren",
+  "Modelo": "Senna",
+  "Año de creación": 2019,
+  "Presente": "Sigue a la venta"
+}
+print(thisdict)
+print("Celulares es de tipo",type(celulares))
+print("Arriba escribe el tipo, en este caso, tupla, de los celulares")
