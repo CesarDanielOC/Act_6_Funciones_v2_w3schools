09b9a50d34736c25aa9b6cececf337839aa9de06
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-13 09:32:32 -0600

    Add files via upload

---
+++ funcionesv2.py
@@ -0,0 +1,20 @@
+print("Funciones version 2")
+print("Olave Cruz")
+celulares=["Uno","Dos","Tres"]
+razasdeperro=["roguaile","bulterie","buldo"]
+
+
+def verlista(telefonos):
+    for uncelular in telefonos: print (uncelular)
+    
+    
+    def verlista2(perros): 
+        for nombres in perros: print (nombres)
+
+
+print("Imprime celulares")
+verlista(celulares)
+
+
+print("Imprime razas de perros")
+verlista(razasdeperro)
